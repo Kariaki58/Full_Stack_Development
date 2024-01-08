1e71ac6640c086d18a1e047aa507289b3b749f71
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-08 20:43:32 +0100

    added blueprints

--- flask_tutorial/flask_blog/__init__.py
+++ flask_tutorial/flask_blog/__init__.py
@@ -2,13 +2,22 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '4d4dc0cd5e830f7356438e980dfeb96f'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
 
 db = SQLAlchemy(app)
@@ -16,8 +25,17 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+mail = Mail(app)
 
 app.app_context().push()
 
 
-from flask_blog import routes
+from flask_blog.users.routes import users
+from flask_blog.main.routes import main
+from flask_blog.posts.routes import posts
+
+
+
+app.register_blueprint(users)
+app.register_blueprint(main)
+app.register_blueprint(posts)
--- flask_tutorial/flask_blog/models.py
+++ flask_tutorial/flask_blog/models.py
@@ -1,4 +1,5 @@
-from flask_blog import db, login_manager
+from flask_blog import db, login_manager, app
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
 from datetime import datetime
 from flask_login import UserMixin
 
@@ -16,6 +17,20 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.secret_key)
+        return s.dumps({'user_id': self.id})
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.secret_key)
+        try:
+            user_id = s.loads(token).user_id
+        except:
+            return None
+        
+        return User.query.get(user_id)
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
@@ -28,4 +43,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.title}', '{self.date_posted}')"
+        return f"Post('{self.title}', '{self.date_posted}')"
